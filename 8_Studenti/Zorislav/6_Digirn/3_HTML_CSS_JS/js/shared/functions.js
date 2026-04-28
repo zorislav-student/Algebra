d17fe372e6084f67fde71a8c2795472e33d33de7
Zori <zorislav.sic1@gmail.com>
2026-04-28 21:05:31 +0200

    Od 28.4.

---
+++ 8_Studenti/Zorislav/6_Digirn/3_HTML_CSS_JS/js/shared/functions.js
@@ -0,0 +1,30 @@
+export function toggleNav(hamburgerEl, navEl) {
+  let navElDisplay = window.getComputedStyle(navEl).display;
+
+  if (navElDisplay === "block") {
+    navEl.classList.remove("open");
+  } else {
+    navEl.classList.add("open");
+  }
+
+  hamburgerEl.classList.toggle("change");
+}
+
+export async function ucitajFirebase() {
+  const tmpNalozi = [];
+
+  const response = await fetch(
+    "https://algebra-fe-default-rtdb.europe-west1.firebasedatabase.app/digirn.json",
+    {
+      method: "GET",
+    },
+  );
+
+  const data = await response.json();
+
+  for (let key in data) {
+    tmpNalozi.push(data[key]);
+  }
+
+  return tmpNalozi[0];
+}
