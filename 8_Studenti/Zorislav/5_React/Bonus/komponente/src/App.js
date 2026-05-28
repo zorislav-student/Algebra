18b9d98da578d082e5d75a0bd1bb6b24cac8e1cf
Zori <zorislav.sic1@gmail.com>
2026-05-28 20:51:07 +0200

    Od 28.5.

--- 8_Studenti/Zorislav/5_React/Bonus/komponente/src/App.js
+++ 8_Studenti/Zorislav/5_React/Bonus/komponente/src/App.js
@@ -2,10 +2,36 @@ import React, { useState } from "react";
 import "./App.css";
 
 class ClassKomponenta extends React.Component {
+  state = {
+    classStanje: "Class pocetno stanje",
+  };
+
+  btnClickHandler() {
+    this.setState({ classStanje: "Class novo stanje" });
+  }
+
   render() {
+    const { classStanje } = this.state;
+    const { ime, starost, grad, children, onAppSt } = this.props;
+
     return (
       <>
-        <h2>Class Komponenta</h2>
+        <br />
+        {classStanje}
+        <h2>{children} Komponenta</h2>
+        <p>Ime: {ime}</p>
+        <p>Starost: {starost}</p>
+        <p>Grad: {grad}</p>
+        <button onClick={this.btnClickHandler.bind(this)}>
+          Promijeni lokalno stanje
+        </button>
+        <button
+          onClick={(event) =>
+            onAppSt("App novo stanje iz class komponente", event)
+          }
+        >
+          Promijeni App stanje
+        </button>
       </>
     );
   }
@@ -27,7 +53,9 @@ function FunkcijskaKomponenta({ ime, starost, grad, children, onAppSt }) {
       <p>Grad: {grad}</p>
       <button onClick={btnClickHandler}>Promijeni lokalno stanje</button>
       <button
-        onClick={(event) => onAppSt("App novo stanje iz funkcijske komponente")}
+        onClick={(event) =>
+          onAppSt("App novo stanje iz funkcijske komponente", event)
+        }
       >
         Promijeni App stanje
       </button>
@@ -38,8 +66,9 @@ function FunkcijskaKomponenta({ ime, starost, grad, children, onAppSt }) {
 function App() {
   const [appStanje, setAppStanje] = useState("App pocetno stanje");
 
-  function appStanjeHandler(novoStanje) {
+  function appStanjeHandler(novoStanje, event) {
     setAppStanje(novoStanje);
+    console.log(event.target);
   }
 
   return (
@@ -54,7 +83,14 @@ function App() {
       >
         Funkcijska
       </FunkcijskaKomponenta>
-      <ClassKomponenta />
+      <ClassKomponenta
+        ime="Marica"
+        starost={33}
+        grad="Pula"
+        onAppSt={appStanjeHandler}
+      >
+        Class
+      </ClassKomponenta>
     </div>
   );
 }
