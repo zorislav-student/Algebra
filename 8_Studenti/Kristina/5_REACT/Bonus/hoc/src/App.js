9c2eddf66ca82b3affe6a87141e38d4e25a5f3d4
KrisQ625 <109870555+KrisQ625@users.noreply.github.com>
2026-06-07 19:00:28 +0200

    HOC

--- 8_Studenti/Kristina/5_REACT/Bonus/hoc/src/App.js
+++ 8_Studenti/Kristina/5_REACT/Bonus/hoc/src/App.js
@@ -1,53 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-//HIOC
-function withLoading(WrappedComponent){
+//HOC
+function withLoading(WrappedComponent) {
+  return function EnhancedComponent({ isloading, ...props }) {
+    if (isloading) {
+      return <p>Loading...</p>;
+    }
 
-return function EnhancedComponent({isloading, ...props}){
-
-  if (isloading){
-return <p>Loading...</p>
-
-  }
-
-  return <WrappedComponent{...props}/>
-}
-
+    return <WrappedComponent {...props} />;
+  };
 }
 
 //Standardna komp
-function UserList({users}){
-return(
-<ul>
-
-{users.map((user)=>(<li key ={user.id}>{user.name}</li>))}
-
-</ul>
-
-);
-
+function UserList({ users }) {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 //Poboljšana komponenta
 
-const EnchancedUserList=withLoading(UserList);
-
+const EnchancedUserList = withLoading(UserList);
 
 function App() {
-
-const users=[
-{id:1,name:"Ana"},
-{id: 2, name:"Marko"}
-
-];
+  const users = [
+    { id: 1, name: "Ana" },
+    { id: 2, name: "Marko" },
+  ];
 
   return (
     <div className="App">
-    <h1 className="">App komponenta</h1>
-    <UserList users={users}/>
-<EnchancedUserList isLoading={true} users={users}/>
-
+      <h1 className="">App komponenta</h1>
+      <UserList users={users} />
+      <EnchancedUserList isLoading={true} users={users} />
     </div>
   );
 }
--- 8_Studenti/Kristina/5_REACT/V17/hoc2/src/App.js
+++ 8_Studenti/Kristina/5_REACT/V17/hoc2/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Button(props) {
+  return <button {...props}></button>;
+}
+function withColor(Element) {
+  return function ({ boja, ...otherProps }) {
+    return <Element style={{ backgroundColor: boja }} {...otherProps} />;
+  };
+}
+
+const ColoredButton = withColor(Button);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={styles.h1stil}>Hello</h1>
+      <ColoredButton boja="red">Bok ja sam crven </ColoredButton>
+      <ColoredButton boja="green">Bok ja sam zelen </ColoredButton>
+      <ColoredButton>Bok ja sam bezbojan </ColoredButton>
+      <p style={styles.pstil}>To sam ja</p>
     </div>
   );
 }
 
 export default App;
+
+const styles = {
+  h1stil: {
+    color: "blue",
+    backgroundColor: "lightgray",
+  },
+  pstil: {
+    color: "yellow",
+  },
+};
